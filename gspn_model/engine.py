290a5eb2dd16d4f83c9d774e996d047ea6dd1f4c
Stefano Marrone <stefano.marrone@unicampania.it>
2024-10-15 00:46:08 +0200

    Engine first version and the execute method

---
+++ gspn_model/engine.py
@@ -0,0 +1,21 @@
+from gspn_model.gspn_naive_handle import generic_analysis
+from utils.configuration import Configuration
+
+
+class Engine:
+    def __init__(self, model, configuration):
+        self.model = model
+        self.model_configuration = configuration
+        config = Configuration()
+        self.gspn_bin_path = config.get('greatspn') #todo: use the parameter and make run general
+
+    def getParamList(self):
+        retval = list()
+        for key in self.model_configuration.keys():
+            retval.extend(['-rpar', key, str(self.model_configuration[key])])
+        return retval
+
+    def execute(self):
+        generic_analysis(self.model, self.getParamList())
+
+
